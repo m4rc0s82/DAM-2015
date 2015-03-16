1d84bbcb1c927801df1aa6219558112920552791
unknown <FPE@965PDS3-008.aeg8.edu>
2015-03-16 14:16:38 +0100

    html5

---
+++ html5-3/js/main.js
@@ -0,0 +1,16 @@
+ window.onload = function(){
+
+    if (Modernizr.input.placeHolder){
+        console.log("YEP");
+    } else {
+        console.log("NOP");
+    }
+
+    Modernizr.load({
+        test: Modernizr.input.placeholder,
+        nope: [
+            'placeholder_polyfill.min.css',
+            'placeholder_polyfill.jquery.min.combo.js'
+        ]
+    });
+};
